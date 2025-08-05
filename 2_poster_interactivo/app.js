5e1d9364bb9f24eda09b4ce95b104e920961b1cf
CuavitasCuak <marincuaava@gmail.com>
2025-08-05 15:01:23 -0500

    Movimiento bloques (Actualización)

--- 2_poster_interactivo/app.js
+++ 2_poster_interactivo/app.js
@@ -1,7 +1,10 @@
+
 const monedas = document.querySelectorAll(".coin");
 const corazon = document.querySelector(".corazon")
 const contadorMonedas = document.querySelector("#contador-monedas");
 const contadorCorazones = document.querySelector("#contador-corazones");
+let totalMonedas = 0;
+let totalCorazones = 0;
 
 console.log(monedas, corazon, contadorMonedas, contadorCorazones);
 
@@ -9,7 +12,15 @@ monedas.forEach(function (moneda) {
     
     moneda.addEventListener("click", function () {
         moneda.classList.add("saltar");
+        totalMonedas++;
+        contadorMonedas.textContent = totalMonedas;
        
     }) 
 
 }) 
+
+corazon.addEventListener("click", function () {
+    corazon.classList.add("saltar");
+    totalCorazones++;
+    contadorCorazones.textContent = totalCorazones;
+})
