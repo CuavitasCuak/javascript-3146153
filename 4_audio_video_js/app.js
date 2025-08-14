d11344a4e82d012fca65026ffddf37c7e15458ed
CuavitasCuak <marincuaava@gmail.com>
2025-08-14 15:32:08 -0500

    audio_video_parte_1

---
+++ 4_audio_video_js/app.js
@@ -0,0 +1,7 @@
+const Video = document.querySelector("#video");
+const Playvideo = document.querySelector("#play-video");
+const Stop = document.querySelector("#stop-video");
+const Playstopimg1 = document.querySelector("#play-stop-img1");
+const Playstopimg2 = document.querySelector("#play-stop-img2");
+
+console.log(Video, Playvideo, Stop, Playstopimg1, Playstopimg2);
