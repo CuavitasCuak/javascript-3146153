92b0dcde35fd79c49cac4ea3d8447adf414740ea
CuavitasCuak <marincuaava@gmail.com>
2025-08-19 15:25:32 -0500

    Audios

--- 4_audio_video_js/app.js
+++ 4_audio_video_js/app.js
@@ -1,7 +1,59 @@
 const Video = document.querySelector("#video");
-const Playvideo = document.querySelector("#play-video");
+const playVideo = document.querySelector("#play-video");
 const Stop = document.querySelector("#stop-video");
 const Playstopimg1 = document.querySelector("#play-stop-img1");
 const Playstopimg2 = document.querySelector("#play-stop-img2");
+const audio1 = document.querySelector("#audio-1");
+const audio2 = document.querySelector("#audio-2");
 
-console.log(Video, Playvideo, Stop, Playstopimg1, Playstopimg2);
+// Play video 
+function reproducirVideo() {
+    Video.play();
+    
+}
+
+
+//evento click para reproducir video
+playVideo.addEventListener("click", reproducirVideo)
+
+
+// Pausar video
+
+function pausarVideo() {
+    Video.pause()
+}
+
+//evento click para pausar video
+
+Stop.addEventListener("click", pausarVideo)
+
+
+//Toggle audio uno
+
+function toggleAudioUno() {
+    if (audio1.paused) {
+        audio1.play();
+        Playstopimg1.textContent = "Stop";
+    } else {
+        audio1.pause();
+        Playstopimg1.textContent = "Play";
+    }   
+
+}
+
+Playstopimg1.addEventListener("click", toggleAudioUno)
+ 
+
+
+function toggleAudioDos() {
+    if (audio2.paused) {
+        audio2.play();
+        Playstopimg2.textContent = "Stop";
+    } else {
+        audio2.pause();
+        Playstopimg2.textContent = "Play";
+    }   
+
+}
+
+Playstopimg2.addEventListener("click", toggleAudioDos)
