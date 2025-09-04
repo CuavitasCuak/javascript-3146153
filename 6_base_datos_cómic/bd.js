b6305b29869305c0442a9808554ea726d43c702f
CuavitasCuak <marincuaava@gmail.com>
2025-09-04 16:51:21 -0500

    Add files via upload

---
+++ 6_base_datos_cómic/bd.js
@@ -0,0 +1,50 @@
+const comic = {
+    "nombreComic": "EL SILENCIO DE ORIEN.",
+    "Categoria": "Apocaliptico",
+    "cantidadCapitulos": 6,
+    "descripcion": "En el planeta llamado Lumara. Orien para el 2017 empieza a tener una sobrepoblación, una elite política empieza a actuar creando una nueva especie para acabar con una pequeña parte de la población, todo eso se sale de control cuando los Drekxon (la nueva especie) toman el poder suficiente para acabar con toda la humanidad. Emilia y Kian dos jóvenes estudiantes de universidad, muy familiares y centrados en su futuro, enfrentan su mayor miedo cuando los Drekxon llegan a la ciudad a acabar con todo.",
+    "Personajes": ["Emilia Kamizi", "Drekxon", "Kian Prescott", "Madre de Emilia", "Padre de Emilia", "Hija de Kian", "Doctor Bernal", "Ministro De Defensa", "Primera Ministra"],
+    "capitulos": [
+        {
+            "nombreCapitulo": "EL DESPERTAR",
+            "imgPortada": "img/capitulos/1.jpg",
+            "Video": "https://www.youtube.com/embed/4pSx1jv1n1g",
+            "descripcion": "Kian Prescott y Emilia Kamizi se conocen accidentalmente en la universidad. Emilia accede a un laboratorio restringido donde descubre organismos extraños que laten sin corazón. Más tarde, un desastre golpea la ciudad: las sirenas de evacuación suenan y la universidad entra en caos. Kian y Emilia se encuentran de nuevo en medio del pánico.",
+            "personajes": ["Emilia Kamizi", "Kian Prescott","Doctor Bernal", "Ministro De Defensa", "Drekxon",]
+        },
+        {
+            "nombreCapitulo": "ECOS DE LA OSCURIDAD",
+            "imgPortada": "img/capitulos/1.jpg",
+            "Video": "https://www.youtube.com/embed/4pSx1jv1n1g",
+            "descripcion": "La ciudad es atacada. Kian y Emilia presencian la primera aparición de los Drekxon, criaturas sin ojos que cazan guiándose por el sonido. Se refugian entre ruinas y sobreviven a duras penas. Descubren que los monstruos no los ven, solo reaccionan al ruido.",
+            "personajes": ["Emilia Kamizi", "Kian Prescott", "Drekxon",]
+        },
+        {
+            "nombreCapitulo": "LUZ ENTRE LAS SOMBRAS",
+            "imgPortada": "img/capitulos/1.jpg", 
+            "Video": "https://www.youtube.com/embed/4pSx1jv1n1g",
+            "descripcion": "Kian y Emilia logran llegar a un refugio abandonado. Allí encuentran un manual militar que confirma que los Drekxon fueron creados por el gobierno y responden a comandos acústicos. Huyen nuevamente por túneles y descubren un centro de datos secreto donde escuchan grabaciones de la Primera Ministra admitiendo la manipulación del pueblo. Deciden revelar la verdad.",
+            "personajes": ["Emilia Kamizi", "Kian Prescott", "Madre de Emilia", "Primera Ministra", "Drekxon"]
+        },
+        {
+            "nombreCapitulo": "ÚLTIMA FRECUENCIA",
+            "imgPortada": "img/capitulos/1.jpg",
+            "Video": "https://www.youtube.com/embed/4pSx1jv1n1g",
+            "descripcion": "Escalan una torre de comunicaciones destruida para transmitir los archivos que incriminan al gobierno. Logran difundir la verdad. En paralelo, un grupo de sobrevivientes en un refugio escucha la transmisión y comienza a recuperar la esperanza. La rebelión contra la manipulación política empieza a gestarse.",
+            "personajes": ["Emilia Kamizi", "Kian Prescott", "Madre de Emilia","Sobrevivientes en un refugio"]
+        },
+        {
+            "nombreCapitulo": "VERDAD OCULTA",
+            "imgPortada": "img/capitulos/1.jpg",
+            "Video": "https://www.youtube.com/embed/4pSx1jv1n1g",
+            "descripcion": "Emilia conecta sus recuerdos: ella misma participó en los experimentos que dieron origen a los Drekxon, sin saberlo. Junto a Kian, decide volver a la universidad para buscar una solución definitiva. Ambos aceptan que su lucha no es solo contra los monstruos, sino contra la élite política que los creó.",
+            "personajes": ["Emilia Kamizi", "Kian Prescott", "Madre de Emilia", "Padre de Emilia", "Doctor Bernal", "Ministro De Defensa"]
+        },
+        {
+            "nombreCapitulo": "SILENCIO DE ACERO",
+            "imgPortada": "img/capitulos/1.jpg",
+            "Video": "https://www.youtube.com/embed/4pSx1jv1n1g",
+            "descripcion": "En el laboratorio destruido, encuentran al Doctor Bernal moribundo, quien les revela la debilidad biológica de los Drekxon: una frecuencia de 17.6 Hz capaz de destruirlos. Emilia y Kian transmiten la señal desde una consola. Los Drekxon comienzan a convulsionar y caer. Los sobrevivientes adoptan el silencio como arma y forma de vida.",
+            "personajes": ["Emilia Kamizi", "Kian Prescott", "Madre de Emilia", "Padre de Emilia", "Doctor Bernal", "Ministro De Defensa"]
+        }
+]}
